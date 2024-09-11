2041452352c0834b4250468cb4e220ed3855f417
IT Compliance <rhodaarthur48@gmail.com>
2024-09-11 14:47:49 -0700

    adds main content layout

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Content1Component } from "./content1/content1.component";
+import { Content2Component } from "./content2/content2.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [Content1Component, Content2Component],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
