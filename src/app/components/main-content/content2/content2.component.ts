514f77e8d638aee03ab7f8f81ed039896f16cd54
IT Compliance <rhodaarthur48@gmail.com>
2024-09-10 21:08:59 -0700

    creates components

---
+++ src/app/components/main-content/content2/content2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content2',
+  standalone: true,
+  imports: [],
+  templateUrl: './content2.component.html',
+  styleUrl: './content2.component.scss'
+})
+export class Content2Component {
+
+}
